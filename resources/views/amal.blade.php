3682904ded386742144164a7796f3ce733a637c6
Amal Samy <baamyaalan55@gmail.com>
2025-07-01 16:02:14 +0300

    first commit

---
+++ resources/views/amal.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {background-color: powderblue;}
+h1   {color: blue;}
+p    {color: red;}
+</style>
+</head>
+<body>
+
+<h1>Learn more</h1>
+<p>Amal samy abo ouda.</p>
+
+</body>
+</html>
