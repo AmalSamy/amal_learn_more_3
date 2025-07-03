d1c80b0bc57c1b5097203c313310ed53ae89cc47
Amal Samy <baamyaalan55@gmail.com>
2025-07-03 16:14:37 +0300

    Route&Controller

---
+++ app/Http/Controllers/SiteOne/SiteOneController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\SiteOne;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SiteOneController extends Controller
+{
+    function home() {
+        echo '<h1> home </h1>';
+    }
+
+    function about() {
+        echo '<h1> about </h1>';
+    }
+
+    function contact() {
+        echo '<h1> contact </h1>';
+    }
+
+    function msg($id){
+        // return $id;
+        return view('SiteOne.msg')->with('id',$id);
+         //توجه الاكشن على  الكنترولر
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,71 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteOne\SiteOneController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/amal', function () {
+/*Route::get('/amal', function () {
     return view ('amal');
+});*/
+
+// Route::get('/home',function(){
+//     return 'Home , Home ';
+// });
+
+//user , profile
+// بدي امرر متغير في رابط واحد لعدة مستخدمين
+// اجباري ارسل id
+// Route::get('/user/profile/{id}',function($id){
+// //return 'user profile';
+// echo 'user id :' . $id;
+// });
+
+/*news , all news , new
+ بدي اعرض خبر معين
+ ليس مجبر انه اعطي id
+ وهيظهر كل الاخبار
+ متغير اختياري
+Route::get('/news/{id?}',function($id = null){
+    //اذا كان في خبر معين بدي اشوفه اعرض هذا الخبر , اذا مافي اعرضلي كل الاخبار
+$news = [1 , 2 , 3 , 6 , 8];
+if($id){
+    echo 'new is :' . $id;
+}else{
+    // بطبع كل الاخبار لو ما اخترت اي دي معين
+foreach ($news as $new) {
+    echo $new . '<br>';
+}
+}
+
 });
+*/
+
+// site 1 / home , about us , contact us
+//prefix بدل ما اضيف الموقع على كل صفحة على حدا , بضيف prefix افضل
+// Route::get('/home', function(){
+// echo '<h1> home </h1>';
+// });
+
+// Route::get('/about', function(){
+// echo '<h1> about us </h1>';
+// });
+
+// Route::get('/contact', function(){
+// echo '<h1> contact us </h1>';
+// });
+
+//راوتس بطريقة الكونترولر
+Route::prefix('site1')->controller(SiteOneController::class)->group(function(){
+Route::get('/home','home');
+Route::get('/about', 'about');
+Route::get('/contact','contact');
+//صفحة للمسجات جاية من الcontact
+Route::get('/msg/{id}','msg');
+
+});
+
+
